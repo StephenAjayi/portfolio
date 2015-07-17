679f4e8ddcd7d5618c5c690f4e2b8d0de7903953
stephen <lonelytreesl@yahoo.com>
2015-07-17 15:58:06 -0700

    add create for Skill.

--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -1,11 +1,46 @@
 class SkillsController < ApplicationController
+
   def index
     @skills = Skill.all
     render :index
   end
 
-  def show
+  def new
+    @skill = Skill.new
+   render :new
+ end
+
+ def create
+   @skill = Skill.new(skill_params)
+   if @skill.save
+     redirect_to  skills_path
+   else
+     render :new
+   end
+ end
+
+ def edit
+   @skill = Skill.find(params[:id])
+   render :edit
+ end
+
+  def update
     @skill = Skill.find(params[:id])
-    render :show
+    if @skill.update(skill_params)
+      redirect_to skills_path
+    else
+      render :edit
+    end
   end
-end
+
+
+ def show
+   @skill = Skill.find(params[:id])
+   render :show
+ end
+
+ private
+ def skill_params
+   params.require(:skill).permit(:name, :description)
+   end
+ end
