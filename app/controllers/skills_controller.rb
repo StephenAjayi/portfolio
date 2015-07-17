68fe83454f3d6d6f89d956bf2126e074ff5b08fd
stephen <lonelytreesl@yahoo.com>
2015-07-17 14:23:23 -0700

    add show and index pages.

---
+++ app/controllers/skills_controller.rb
@@ -0,0 +1,11 @@
+class SkillsController < ApplicationController
+  def index
+    @skills = Skill.all
+    render :index
+  end
+
+  def show
+    @skill = Skill.find(params[:id])
+    render :show
+  end
+end
