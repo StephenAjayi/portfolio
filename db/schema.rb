3efd43e4641f3910563cde2f863ef00f949a4b3b
stephen <lonelytreesl@yahoo.com>
2015-07-18 20:52:10 -0700

    add spec and validation of name for Project, all specs passing,

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150717175308) do
+ActiveRecord::Schema.define(version: 20150719033117) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "projects", force: :cascade do |t|
+    t.string   "name"
+    t.string   "description"
+    t.integer  "skill_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "skills", force: :cascade do |t|
     t.string   "name"
