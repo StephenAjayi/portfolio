e06794bbe7cb45a52408df3007c9a6239ccaa2ac
stephen <lonelytreesl@yahoo.com>
2015-07-19 01:56:59 -0700

    add url to projects.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150719033117) do
+ActiveRecord::Schema.define(version: 20150719082621) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20150719033117) do
     t.integer  "skill_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "url"
   end
 
   create_table "skills", force: :cascade do |t|
