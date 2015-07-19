f4a94fb081d330815fe01d2c04197e26d154738a
stephen <lonelytreesl@yahoo.com>
2015-07-18 21:05:25 -0700

    add spec and code for  Project belongs to skill association, all specs passing.

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,6 @@
 class Project < ActiveRecord::Base
+  belongs_to :skill
 
   validates :name, :presence => true
-  validates :description, :presence => true 
+  validates :description, :presence => true
 end
