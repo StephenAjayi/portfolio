3e4764b76212f2806d7e02d5de34ca22e80ae89f
stephen <lonelytreeslyahoo.com>
2015-07-20 00:58:44 -0700

    add spec and code to test edit skill process, all specs passing .

---
+++ spec/features/edit_skill_pages_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+describe "the edit a skill process" do
+  it "edits a skill" do
+    skill = Skill.create(:name => 'Ruby', :description => "ruby is..")
+    visit skill_path(skill)
+    click_on 'Edit skill'
+    fill_in 'Name', :with => 'Rails'
+    fill_in 'Description', :with => 'Rails is ...'
+    click_on 'Update Skill'
+    expect(page).to have_content 'Skill successfully updated!'
+  end
+end
