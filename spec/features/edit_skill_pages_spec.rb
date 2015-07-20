f796fc9884835bd98511cc0984d67c1e2f9724e8
stephen <lonelytreeslyahoo.com>
2015-07-20 01:01:44 -0700

    add spec and code to test error is given if no description is present during update, all specs passing .

--- spec/features/edit_skill_pages_spec.rb
+++ spec/features/edit_skill_pages_spec.rb
@@ -10,4 +10,14 @@ describe "the edit a skill process" do
     click_on 'Update Skill'
     expect(page).to have_content 'Skill successfully updated!'
   end
+
+  it "gives error when no description is entered" do
+    skill = Skill.create(:name => 'Ruby', :description => "ruby is..")
+    visit skill_path(skill)
+    click_on 'Edit skill'
+    fill_in 'Name', :with => 'Rails'
+    fill_in 'Description', :with => ''
+    click_on 'Update Skill'
+    expect(page).to have_content 'errors'
+  end
 end
